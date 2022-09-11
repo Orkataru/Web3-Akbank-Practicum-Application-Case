807f08ff9ae76a90d2c1cf45e2f4f6e39c50c383
Orkataru <91630525+Orkataru@users.noreply.github.com>
2022-09-11 13:09:00 +0300

    Add files via upload

---
+++ js_oddoreven.js
@@ -0,0 +1,24 @@
+function oddoreven(a){
+    let total = 0;
+    var a_str = a.toString();
+    var len = a_str.length;
+
+    for (let i = 0; i < len  ; i+=1){
+        let num = a_str[i];
+        num = parseInt(num);
+        total += num;
+
+    } 
+
+
+if (total%2 == 1){
+    return "Odd";
+}
+
+else if (total%2 == 0){
+    return "Even";
+}
+
+
+
+}
